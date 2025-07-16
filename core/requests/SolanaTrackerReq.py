b871edf115d2daa8ebf36f8a8d3bef8b996cb218
Alexandr Beliakov <genzksync0@gmail.com>
2025-07-16 10:57:00 +0300

    create SolanaTracker request model

---
+++ core/requests/SolanaTrackerReq.py
@@ -0,0 +1,33 @@
+import logging
+
+from core.exceptions import ApiError, HttpStatusError, NetworkError, JsonResponseError
+from core.requests.request_model import BaseRequest
+
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+class STRequest(BaseRequest):
+
+    async def fetch(self, url: str) -> dict:
+
+        try:
+            data = self._raw_get(url=url)
+
+            if not isinstance(data, dict):
+                raise ApiError(f"Expected {dict.__name__}, got {type(data).__name__}")
+
+            return data
+
+        except HttpStatusError as e:
+            logger.error("HTTP Status Error %s for %s", e.status, e.url)
+            return {}
+
+        except NetworkError as e:
+            logger.error("Network Error: ", e)
+            return {}
+
+        except JsonResponseError as e:
+            logger.error("Parse JSON error from %s", url, e)
+            return {}
