f39f80412e6f266a01231a3bfd3d2d9b72c13f42
Alexandr Beliakov <genzksync0@gmail.com>
2025-07-16 10:58:00 +0300

    update solana_tracker_api

--- api_v1/crypto/solana_tracker/solana_tracker_api.py
+++ api_v1/crypto/solana_tracker/solana_tracker_api.py
@@ -1,8 +1,12 @@
 import logging
+from typing import Any
 
 import aiohttp
 
 from api_v1.crypto.solana_tracker.config import settings
+from api_v1.crypto.solana_tracker.utils import parse_data_st
+from core.exceptions import ApiError, JsonParseError
+from core.requests import STRequest
 
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
@@ -16,53 +20,31 @@ class SolanaTrackerApi:
         session: aiohttp.ClientSession,
         header: dict = settings.header,
     ) -> None:
-        self.tokenAddress = token_address
+        self.contract_address = token_address
         self.header = header
         self.session = session
 
-    async def get_info_about_token(
-        self, url: str = settings.token_pair_url
-    ) -> dict | None:
+    async def fetch_token_info(
+        self,
+        client: STRequest,
+        url: str = settings.token_info_url,
+    ) -> dict:
+        token_info_url = url + self.contract_address
+        try:
+            return await client.fetch(url=token_info_url)
+        except ApiError as e:
+            logger.error("ApiError fetch_token_info SolanaTracker: ", e)
+            return {}
+
+    async def get_info_about_token(self) -> dict[str, Any]:
+        client = STRequest(session=self.session)
+        st_data = await self.fetch_token_info(client=client)
 
         try:
-            async with self.session.get(
-                url=url, headers=self.header
-            ) as response_from_solana_tracker:
-                if response_from_solana_tracker.status != 200:
-                    logger.error(
-                        "Ошибка запроса к Solana Tracker API. Статус: %s",
-                        response_from_solana_tracker.status,
-                    )
-                    return None
+            parsed_data = parse_data_st(st_data)
 
-                data = await response_from_solana_tracker.json()
-
-                if data:
-                    name = data.get("token", {}).get("name", None)
-                    symbol = data.get("token", {}).get("symbol", None)
-                    image = data.get("token", {}).get("image", None)
-                    extensions = data.get("token", {}).get("extensions", None)
-                    holders = data.get("holders", None)
-                    pools_list = data.get("pools", None)
-
-                poolId = ""
-                token_supply = None
-
-                if pools_list and isinstance(token_supply, list):
-                    poolId = pools_list[0].get("poolId", None)
-                    token_supply = pools_list[0].get("tokenSupply", None)
-
-                return {
-                    "name": name,
-                    "symbol": symbol,
-                    "image": image,
-                    "extensions": extensions,
-                    "holders": holders,
-                    "poolId": poolId,
-                    "token_supply": token_supply,
-                }
-
-        except Exception as e:
-            logger.exception("Ошибка при получение Json объекта ", e)
-
+            if parsed_data:
+                return parsed_data
+        except JsonParseError as e:
+            logger.error("Error parse json data from SolanaTracker: ", e)
             return settings.defaults
