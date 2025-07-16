bafefdbff0d3a0496cd1706ebc061b150ba6c527
Alexandr Beliakov <genzksync0@gmail.com>
2025-07-16 11:25:47 +0300

    changed variable name for url in config solana tracker

--- api_v1/crypto/solana_tracker/config.py
+++ api_v1/crypto/solana_tracker/config.py
@@ -5,19 +5,19 @@ from env import X_API_KEY_FOR_ST
 
 class BaseSTSettings(BaseSettings):
 
-    token_pair_url: str = "https://data.solanatracker.io/tokens/"
+    token_info_url: str = "https://data.solanatracker.io/tokens/"
 
     header = {
         "x-api-key": X_API_KEY_FOR_ST,
     }
 
     defaults = {
-        "name": None,
-        "symbol": None,
-        "image": None,
+        "name": "",
+        "symbol": "",
+        "image": "",
         "extensions": None,
-        "holders": None,
-        "poolId": None,
+        "holders": [],
+        "poolId": "",
         "token_supply": None,
     }
 
