9380df937c3de52dfc1f397772f9968f2de3ba9f
Alexandr Beliakov <genzksync0@gmail.com>
2025-07-16 10:58:24 +0300

    create Moralis request model

---
+++ core/requests/MoralisRequest.py
@@ -0,0 +1,43 @@
+import logging
+
+import aiohttp
+
+from core import BaseRequest
+from core.exceptions import JsonResponseError, NetworkError, HttpStatusError, ApiError
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+class MoralisRequest(BaseRequest):
+
+    async def fetch(self, url: str) -> str:
+
+        try:
+            data = await self._raw_get(url=url)
+
+            if not isinstance(data, dict):
+                raise ApiError(f"Expected {dict.__name__}, got {type(data).__name__}")
+
+            list_pa = data.get("pairs", [])
+
+            if list_pa and isinstance(list_pa, list):
+                first_pa = list_pa[0]
+
+                if isinstance(first_pa, dict):
+                    pa = first_pa.get("pairAddress", "")
+                    return pa
+
+            return ""
+
+        except HttpStatusError as e:
+            logger.error("HTTP Status Error %s for %s", e.status, e.url)
+            return ""
+
+        except NetworkError as e:
+            logger.error("Network Error: ", e)
+            return ""
+
+        except JsonResponseError as e:
+            logger.error("Parse JSON error from %s", url, e)
+            return ""
